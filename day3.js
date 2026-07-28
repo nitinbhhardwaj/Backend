63ed7176fe5200a79b5f7f14d29c7298692c4390
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-07-28 09:06:48 +0530

    Promises in Javascript

---
+++ day3.js
@@ -0,0 +1,6 @@
+//Promises - The promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value. A Promise is in one of these states:
+//Pending:initial state, neither fulfilled nor rejected.
+//Fulfilled: meaning that the operation completed successfully.
+//Rejected: meaning that the operation failed.
+
+
