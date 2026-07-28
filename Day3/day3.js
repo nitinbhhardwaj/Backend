fafe44c25d0e8f809c7faa24bdc461c6146a20bc
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-07-28 10:56:28 +0530

    Question on Promise usage

--- Day3/day3.js
+++ Day3/day3.js
@@ -30,10 +30,12 @@
 
 //Async Await
 console.log("1");
-//async function test(){
-    // console.log("2");
-    // await console.log("3");
-    // console.log("4");
-//}
-// test();
+async function test(){
+    console.log("2");
+    await console.log("3");
+    console.log("4");
+}
+t1=test();
 console.log("5");
+
+//Create promise that will print username and password using and if useraname and password not found then it will call rehect state and print error.......
