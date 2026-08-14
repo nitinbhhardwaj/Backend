95381088985d62026582078efc8e25490bdf7fef
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-08-14 14:07:44 +0530

    Event Emitter

--- Eventmodule/program1.js
+++ Eventmodule/program1.js
@@ -2,20 +2,22 @@
 //EventEmitter- register event or event listener, on(),emit(event param)-trigger event/create event/fire event
 
 const EventEmitter=require('events');
-const event=new EventEmitter();
-event.on("greet",()=>{
-    console.log("this is event listener");
+class MyEvent extends EventEmitter{}
+const events=new MyEvent();
+event.on("greet",(name)=>{
+    console.log(`hello cse 24 my name is ${name}`);//template literals-`${vari}`
 });
-event.emit("greet");
+events.on("exit",()=>{})
+event.emit("greet","Nitin Bhardwaj");
 
-const even=new EventEmitter();
-event.on("name",()=>{
-    console.log("Name:Nitin Bhardwaj");
-});
-event.emit("name");
+// const even=new EventEmitter();
+// event.on("name",()=>{
+//     console.log("Name:Nitin Bhardwaj");
+// });
+// event.emit("name");
 
-const eve=new EventEmitter();
-event.on("age",()=>{
-    console.log("Age:36");
-});
-event.emit("age");
+// const eve=new EventEmitter();
+// event.on("age",()=>{
+//     console.log("Age:36");
+// });
+// event.emit("age");
---
+++ Eventmodule/program2.js
@@ -0,0 +1,4 @@
+//Create trigger event click and mouseover and call that mouseover event in click event
+class button{
+    
+}
