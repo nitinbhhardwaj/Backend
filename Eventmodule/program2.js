b381d23fb96413276d00263a4e4b91c22f3c2b21
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-08-18 11:08:22 +0530

    passing parameters

--- Eventmodule/program2.js
+++ Eventmodule/program2.js
@@ -1,19 +1,34 @@
+// const EventEmitter = require("events");
+
+// const emitter = new EventEmitter();
+
+// // mouseover event
+// emitter.on("mouseover", () => {
+//     console.log("Mouseover event triggered");
+// });
+
+// // click event
+// emitter.on("click", () => {
+//     console.log("Click event triggered");
+
+//     // Trigger mouseover inside click
+//     emitter.emit("mouseover");
+// });
+
+// // Trigger click
+// emitter.emit("click");
+
 const EventEmitter = require("events");
 
-const emitter = new EventEmitter();
+const name = "Nitin Bhardwaj";
 
-// mouseover event
-emitter.on("mouseover", () => {
+const emitter=new EventEmitter();
+
+emitter.on("click",()=>{
+    console.log(`Click event triggered for ${name}`);
+});
+emitter.on("mouseover",()=>{
     console.log("Mouseover event triggered");
 });
-
-// click event
-emitter.on("click", () => {
-    console.log("Click event triggered");
-
-    // Trigger mouseover inside click
-    emitter.emit("mouseover");
-});
-
-// Trigger click
-emitter.emit("click");
+emitter.emit('click');
+emitter.emit('mouseover');
