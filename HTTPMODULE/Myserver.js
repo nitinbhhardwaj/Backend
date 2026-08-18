98cb346d7cb51a29661ddc6019ce4b26ca1b900a
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-08-18 11:33:41 +0530

    Create a server using http module

--- HTTPMODULE/Myserver.js
+++
@@ -1,7 +0,0 @@
-//Create my own server using http module
-import http from "http";
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
-});
