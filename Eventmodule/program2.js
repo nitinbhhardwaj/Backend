6df401e0503faad24782f0b7e3108f8b49fbaeaa
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-08-18 10:56:26 +0530

    Mouseover event triggered

--- Eventmodule/program2.js
+++ Eventmodule/program2.js
@@ -2,3 +2,10 @@
 //Simulate DOM like event handling in Node.js using event emitter
 //addEventListener-.on()
 //dispatchEvent-.emit()
+const EventEmitter=require('events');
+const emitter=new EventEmitter();
+emitter.on('mouseover',()=>{
+    console.log('Mouseover event triggered');
+});
+emitter.emit('click');
+emitter.emit('mouseover');
