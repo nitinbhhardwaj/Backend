acbb3ed2dc5b97fc8edcc08b69bdbb13800892f8
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-08-18 11:16:20 +0530

    Comment added for understanding

--- Eventmodule/program2.js
+++ Eventmodule/program2.js
@@ -20,15 +20,17 @@
 
 const EventEmitter = require("events");
 
-const name = "Nitin Bhardwaj";
+
 
 const emitter=new EventEmitter();
 
-emitter.on("click",()=>{
+emitter.on("click",(name)=>{
     console.log(`Click event triggered for ${name}`);
 });
 emitter.on("mouseover",()=>{
     console.log("Mouseover event triggered");
 });
-emitter.emit('click');
+emitter.emit('click','Nitin Bhardwaj');
 emitter.emit('mouseover');
+
+//click is triggered event and if you pass any variable as an argument, it will be received by the event listener
