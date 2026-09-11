dd9dc153462e85d4e7c483f447611c8c883f369b
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-09-11 12:49:19 +0530

    Add project files

---
+++ ExpressHTML/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const app=express();
+const PORT=3000;
+app.get('/',(req,res)=>{
+    fs.readFile('./pages/index.html','utf-8',(err,data)=> {
+        if(err){
+            res.status(500).send('Error reading the file');
+            return;
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+app.get('/about',(req,res)=>{
+    fs.readFile('/pages/about.html','utf-8',(err,data)=> {
+        if(err){
+            res.status(500).send('Error reading file');
+            return;
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+app.get('/contact',(req,res)=>{
+    fs.readFile('./pages/contact.html','utf-8',(err,data)=> {
+        if(err){
+            res.status(500).send('Error reading file');
+            return;
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
