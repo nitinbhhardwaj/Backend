250d8957c8b4d782db6702ac2c92a789353400ea
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-09-11 13:04:29 +0530

    Backend(express) with front-end(HTML)

--- ExpressHTML/index.js
+++ ExpressHTML/index.js
@@ -1,40 +1,39 @@
 import express from 'express';
+import fs from 'fs';
 
-const app=express();
-const PORT=3000;
-app.get('/',(req,res)=>{
-    fs.readFile('./pages/index.html','utf-8',(err,data)=> {
-        if(err){
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    fs.readFile('./pages/index.html', 'utf-8', (err, data) => {
+        if (err) {
             res.status(500).send('Error reading the file');
             return;
         }
-        else {
-            res.send(data);
-        }
+        res.send(data);
     });
 });
-app.get('/about',(req,res)=>{
-    fs.readFile('/pages/about.html','utf-8',(err,data)=> {
-        if(err){
-            res.status(500).send('Error reading file');
+
+app.get('/about', (req, res) => {
+    fs.readFile('./pages/about.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading the file');
             return;
         }
-        else {
-            res.send(data);
-        }
+        res.send(data);
     });
 });
-app.get('/contact',(req,res)=>{
-    fs.readFile('./pages/contact.html','utf-8',(err,data)=> {
-        if(err){
-            res.status(500).send('Error reading file');
+
+app.get('/contact', (req, res) => {
+    fs.readFile('./pages/contact.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading the file');
             return;
         }
-        else {
-            res.send(data);
-        }
+        res.send(data);
     });
 });
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
