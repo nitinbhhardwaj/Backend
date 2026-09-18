e1994d475ba74136efbb606226655c6a01c1ba58
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-09-18 12:44:55 +0530

    Changes in app.jsx

--- Assignments/A4/Express_React/Product-ReactAPI/src/App.jsx
+++ Assignments/A4/Express_React/Product-ReactAPI/src/App.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+function App() {
+  const [products, setProducts] = useState([]);
 
-export default App
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // GET
+  const getProducts = async () => {
+    const res = await fetch("http://localhost:5000/api/products");
+    const data = await res.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  // POST
+  const addProduct = async (e) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:5000/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, price })
+    });
+
+    setName("");
+    setPrice("");
+    getProducts();
+  };
+
+  // DELETE
+  const deleteProduct = async (id) => {
+    await fetch(`http://localhost:5000/api/products/${id}`, {
+      method: "DELETE"
+    });
+
+    getProducts();
+  };
