9c26bec67df546706b7c6f8fbe605bbab92f2218
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-09-25 13:05:49 +0530

    Changes in App.jsx

--- Express_search/search_app/src/App.jsx
+++ Express_search/search_app/src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
-const App= () => {
-  const documents=[
-    {name:"pdfName",
-      file:"Abc"
+
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  const documents = [
+    {
+      name: "abc",
+      file: "abc.docx"
     }
-  ]
-}
+  ];
+
+  return (
+    <div>
+      <h1>Notes Portal App</h1>
+
+      <input
+        type="text"
+        placeholder="search notes here"
+        onClick={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default App;
